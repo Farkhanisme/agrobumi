0f25dbeb1b6ceecc9b0d17de476767472a8972d7
fakhriyyah <fakhriyyatul.fauziyyah@gmail.com>
2024-11-11 10:15:18 +0700

    dari fakhriyyah

---
+++ src/pages/About.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function AboutUs() {
+return (
+    <div className="container">
+      <header>
+        <h2>Narmada Botanic Garden</h2>
+      </header>
+
+      <main>
+        <section>
+          <h2>Tentang Kami</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <h2>Visi</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <h2>Misi</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </section>
+
+        
+      </main>
+
+      <footer>
+        <p>&copy; 2023 Narmada Botanic Garden</p>
+      </footer>
+    </div>
+  );
+}
+
+export default AboutUs;
