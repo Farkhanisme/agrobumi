60fa92963d7eb8f2e5d3d81c482dca967f3dcc9d
Farkhanisme <farkhanmahmud@gmail.com>
2024-11-11 11:49:38 +0700

    penambahan footer

--- src/pages/Us.jsx
+++ src/pages/Us.jsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React from "react";
 
 function TeamAgrobumi() {
   return (
     <div className="container">
       <h1>Team Agrobumi Indonesia</h1>
       <div className="level">
-        <div className="person" style={{ backgroundImage: `url('../fay.jpg')` }} />
+        <div
+          className="person"
+          style={{ backgroundImage: `url('/fay.jpg')` }}
+        />
       </div>
       <div className="level">
-        <div className="person" style={{ backgroundImage: `url('../farhan.jpg')` }} />
-        <div className="person" style={{ backgroundImage: `url('../fakhriyyah.jpg')` }} />
-        <div className="person" style={{ backgroundImage: `url('../person1.jpg')` }} />
+        <div
+          className="person"
+          style={{ backgroundImage: `url('/farhan.jpg')` }}
+        />
+        <div
+          className="person"
+          style={{ backgroundImage: `url('/fakhriyyah.jpg')` }}
+        />
+        <div
+          className="person"
+          style={{ backgroundImage: `url('/person1.jpg')` }}
+        />
       </div>
       <div className="level">
-        <div className="person" style={{ backgroundImage: `url('../person2.jpg')` }} />
-        <div className="person" style={{ backgroundImage: `url('../person3.jpg')` }} />
-        <div className="person" style={{ backgroundImage: `url('../person1.jpg')` }} />
+        <div
+          className="person"
+          style={{ backgroundImage: `url('/person2.jpg')` }}
+        />
+        <div
+          className="person"
+          style={{ backgroundImage: `url('/person3.jpg')` }}
+        />
+        <div
+          className="person"
+          style={{ backgroundImage: `url('/person1.jpg')` }}
+        />
       </div>
     </div>
   );
 }
 
 export default TeamAgrobumi;
-
