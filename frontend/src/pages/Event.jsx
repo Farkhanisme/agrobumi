965cea5d59c9bf59977943c7ccb6450a7e2cfd32
Farkhanisme <farkhanmahmud@gmail.com>
2024-12-09 08:44:46 +0700

    cek ammar

--- frontend/src/pages/Event.jsx
+++ frontend/src/pages/Event.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Event.css";
+import { use } from "react";
 
 const App = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [popupContent, setPopupContent] = useState("");
+  const [popupHeader, setPopupHeader] = useState("");
+  const [popupContent_1, setPopupContent_1] = useState("");
+  const [popupContent_2, setPopupContent_2] = useState("");
+  const [popupContent_3, setPopupContent_3] = useState("");
 
-  const handleDetailClick = (card, content) => {
-    setPopupContent(content);
+  const handleDetailClick = (card, content_1, content_2, content_3) => {
+    setPopupHeader(card);
+    setPopupContent_1(content_1);
+    setPopupContent_2(content_2);
+    setPopupContent_3(content_3);
     setIsPopupOpen(true);
   };
 
@@ -99,7 +106,7 @@ const App = () => {
         </div>
       </div>
 
-      <div className="container" id="explore">
+      <div className="mx-auto p-10" id="explore">
         <div className="card-container my-20">
           <div className="card">
             <img
@@ -114,17 +121,21 @@ const App = () => {
             <a>Lokasi</a>
 
             <div className="flex justify-end space-x-5 border-t-2 py-5">
-              <button
-                className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4"
-                onClick={() =>
-                  handleDetailClick(
-                    "card1",
-                    "Acara Memasak masakan Tradisional untuk para pelancong mancanegara"
-                  )
-                }
-              >
-                Detail
-              </button>
+              <Link>
+                <button
+                  className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4"
+                  onClick={() =>
+                    handleDetailClick(
+                      "Cooking Class",
+                      "Acara Memasak masakan Tradisional untuk para pelancong mancanegara",
+                      "Biaya: 450,000/group(include bahan masakan)",
+                      "Catatan: Peserta bisa menikmati hasil masakannya sendiri atau membawanya pulang sebagai oleh-oleh."
+                    )
+                  }
+                >
+                  Detail
+                </button>
+              </Link>
               <Link to="/booking">
                 <button className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4">
                   Pesan
@@ -145,17 +156,21 @@ const App = () => {
             <a>Family Plan</a>
             <a>Lokasi</a>
             <div className="flex justify-end space-x-5 border-t-2 py-5">
-              <button
-                className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4"
-                onClick={() =>
-                  handleDetailClick(
-                    "card2",
-                    "Education Class di Narmada Botanic Garden menggabungkan pembelajaran dengan pengalaman alam terbuka."
-                  )
-                }
-              >
-                Detail
-              </button>
+              <Link>
+                <button
+                  className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4"
+                  onClick={() =>
+                    handleDetailClick(
+                      "Education Class",
+                      "Education Class di Narmada Botanic Garden menggabungkan pembelajaran dengan pengalaman alam terbuka.",
+                      "Mengenal Jenis Tanaman Tropis dan Manfaatnya.",
+                      "Biaya: 350,00/group (termasuk semua perlengkapan dan sertifikat partisipasi)."
+                    )
+                  }
+                >
+                  Detail
+                </button>
+              </Link>
               <Link to="/booking">
                 <button className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4">
                   Pesan
@@ -176,17 +191,29 @@ const App = () => {
             <a>Family Plan</a>
             <a>Lokasi</a>
             <div className="flex justify-end space-x-5 border-t-2 py-5">
-              <button
-                className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4"
-                onClick={() =>
-                  handleDetailClick(
-                    "card3",
-                    "Narmada Botanic Garden menawarkan ruang untuk acara privat seperti garden party, pesta ulang tahun, reuni keluarga, hingga acara perusahaan."
-                  )
-                }
-              >
-                Detail
-              </button>
+              <Link>
+                <button
+                  className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4"
+                  onClick={() =>
+                    handleDetailClick(
+                      "Garden Party",
+                      "Narmada Botanic Garden menawarkan ruang untuk acara privat seperti garden party, pesta ulang tahun, reuni keluarga, hingga acara perusahaan.",
+                      `<ul class='list-disc'>
+                        <li>Paket 1: Untuk 10-20 orang, Rp2.000.000 (termasuk dekorasi sederhana).</li>
+                        <li>Paket 2: Untuk 20-50 orang, Rp5.000.000 (termasuk dekorasi, sound system, dan area eksklusif).</li>
+                        <li>Paket 3/Premium: Untuk lebih dari 50 orang, mulai dari Rp10.000.000 (termasuk dekorasi tematik, sound system, dan pencahayaan khusus).</li>
+                      </ul>`,
+                      `Fasilitas Tambahan: </br>
+                      <ul class='list-disc'>
+                        <li>Catering dengan hidangan tradisional atau internasional.</li>
+                        <li>Sewa fotografer khusus untuk dokumentasi acara.</li>
+                      </ul>`
+                    )
+                  }
+                >
+                  Detail
+                </button>
+              </Link>
               <Link to="/booking">
                 <button className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4">
                   Pesan
@@ -207,17 +234,28 @@ const App = () => {
             <a>Family Plan</a>
             <a>Lokasi</a>
             <div className="flex justify-end space-x-5 border-t-2 py-5">
-              <button
-                className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4"
-                onClick={() =>
-                  handleDetailClick(
-                    "card4",
-                    "Para pengunjung dapat merasakan atmosfer khas Korea dan Jepang melalui berbagai spot foto tematik yang kami disediakan."
-                  )
-                }
-              >
-                Detail
-              </button>
+              <Link>
+                <button
+                  className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4"
+                  onClick={() =>
+                    handleDetailClick(
+                      "Spot Foto ala ala korea Jepang",
+                      "Para pengunjung dapat merasakan atmosfer khas Korea dan Jepang melalui berbagai spot foto tematik yang kami disediakan.",
+                      `<ul class='list-disc'>
+                        <li>Set taman bergaya Jepang dengan bunga sakura.</li>
+                        <li>Area ala hanok (rumah tradisional Korea) untuk nuansa Korea.</li>
+                        <li>Aksesori tradisional seperti payung Jepang dan Hanbok untuk menambah keseruan sesi foto.</li>
+                      </ul>`,
+                      `<ul class='list-disc'>
+                        <li>Harga Tiket: Rp50.000 per orang (termasuk akses ke area spot foto dan peminjaman aksesori sederhana).</li>
+                        <li>Harga Ticket Biasa:10.000/orang(biaya masuk biasa tanpa perlengkapan)</li>
+                      </ul>`
+                    )
+                  }
+                >
+                  Detail
+                </button>
+              </Link>
               <Link to="/booking">
                 <button className="outline outline-1 outline-black bg-white text-blue-800 rounded-md py-1 px-4">
                   Pesan
@@ -230,12 +268,34 @@ const App = () => {
 
       {isPopupOpen && (
         <div className="popup">
-          <div className="popup-content">
-            <button className="close-btn" onClick={closePopup}>
-              &times;
-            </button>
-            <h2>Detail Event</h2>
-            <p>{popupContent}</p>
+          <div className="popup-content p-5">
+            <h1 className="text-center text-succes-3 text-3xl">
+              Narmada <span className="text-secondary-2">Botanic</span> Garden
+            </h1>
+            <br />
+            <h2 className="font-semibold text-left">{popupHeader}</h2>
+            <div className="popup-body mx-14">
+              <p className="border-b-2 py-3">{popupContent_1}</p>
+              <p
+                className="border-b-2 py-3"
+                dangerouslySetInnerHTML={{ __html: popupContent_2 }}
+              ></p>
+              <p className="border-b-2 py-3" dangerouslySetInnerHTML={{ __html: popupContent_3 }}></p>
+              <div className="popup-button flex flex-row justify-end items-center space-x-5  my-3">
+                <button
+                  className="outline outline-1 outline-danger-1 text-danger-1 py-1 px-5 rounded-md"
+                  onClick={closePopup}
+                >
+                  Tutup
+                </button>
+                <Link
+                  to="/booking"
+                  className="outline outline-1 outline-button-1 text-button-1 py-1 px-5 rounded-md"
+                >
+                  Pesan
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       )}
